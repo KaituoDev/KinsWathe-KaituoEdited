d4e9e0b580ea85c9e43cb3e41d68ffc6d2c0063a
BsXinQin <3482063535@qq.com>
2026-02-26 20:28:08 +0800

    1.5.1

--- src/client/java/org/BsXinQin/kinswathe/client/mixin/roles/bodymaker/BodymakerDoNotCloseMixin.java
+++ src/client/java/org/BsXinQin/kinswathe/client/mixin/roles/bodymaker/BodymakerDoNotCloseMixin.java
@@ -2,11 +2,16 @@ package org.BsXinQin.kinswathe.client.mixin.roles.bodymaker;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import dev.doctor4t.wathe.cca.GameWorldComponent;
+import dev.doctor4t.wathe.client.WatheClient;
 import dev.doctor4t.wathe.client.gui.screen.ingame.LimitedHandledScreen;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
+import org.BsXinQin.kinswathe.KinsWatheRoles;
 import org.BsXinQin.kinswathe.client.roles.bodymaker.BodymakerRoleWidget;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,8 +24,20 @@ public class BodymakerDoNotCloseMixin {
         if (BodymakerRoleWidget.stopClosing) return false;
         return original.call(instance,keyCode,scanCode);
     }
+
     @Inject(method = "close", at = @At("HEAD"))
     void setStopClosingToFalse(CallbackInfo ci) {
         BodymakerRoleWidget.stopClosing = false;
+    }
+
+    @Unique
+    public void reset() {
+        if (MinecraftClient.getInstance().player == null) return;
+        GameWorldComponent gameWorld = GameWorldComponent.KEY.get(MinecraftClient.getInstance().player.getWorld());
+        if (BodymakerRoleWidget.stopClosing) {
+            if (WatheClient.isPlayerSpectatingOrCreative() || !gameWorld.isRole(MinecraftClient.getInstance().player, KinsWatheRoles.BODYMAKER)) {
+                BodymakerRoleWidget.stopClosing = false;
+            }
+        }
     }
 }
