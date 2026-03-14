3ae53d71af0bcfa0fb470085e3d668364c7782c6
APairOfMoons <wangyupeng198@yeah.net>
2026-03-14 21:30:34 +0800

    Fix the bug that the death reason of death penalty didn't displayed correctly.

--- src/main/java/org/BsXinQin/kinswathe/KinsWatheDeathReasons.java
+++ src/main/java/org/BsXinQin/kinswathe/KinsWatheDeathReasons.java
@@ -1,8 +1,7 @@
 package org.BsXinQin.kinswathe;
 
 import net.minecraft.util.Identifier;
-import org.agmas.noellesroles.Noellesroles;
 
 public class KinsWatheDeathReasons {
-    public static Identifier DEATH_PENALTY_DEATH_REASON = Identifier.of(Noellesroles.MOD_ID, "death_penalty");
+    public static Identifier DEATH_PENALTY_DEATH_REASON = Identifier.of(KinsWathe.MOD_ID, "death_penalty");
 }
